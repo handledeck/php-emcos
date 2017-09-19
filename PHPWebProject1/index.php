7146982284cc71bd9c0e4f58290ad9b8090c42f8
aleh <aleh.dronov@gmail.com>
2017-09-19 11:01:52 +0300

    read data

--- PHPWebProject1/index.php
+++ PHPWebProject1/index.php
@@ -1,8 +1,8 @@
 <?php
 $em=new emcos();
 $exp= $em->connect();
-$em->fill();
-var_dump($em->tree);
+//$em->fill();
+//var_dump($em->tree);
 $z=0;
 class emcos{
 
@@ -14,62 +14,78 @@ class emcos{
     private $conn=null;
     public  $tree;
 
-    function __construct(string $host="10.208.4.83",string $service="emcor",string $username="emcos",string $password="emc05",int $id=49){
+    function __construct(string $host="10.208.4.83",string $service="emcor",string $username="sau",string $password="sau",int $id=49){
         $this->db_username=$username;
         $this->db_password=$password;
         $this->db_host=$host;
+        $this->service=$service;
         $this->tns="(DESCRIPTION =(ADDRESS_LIST=(ADDRESS = (PROTOCOL = TCP)(HOST=".$host.")(PORT = 1521)))(CONNECT_DATA =(SERVICE_NAME=".$service.")))";
         $this->tree=new emc("root",$id);
     }
 
-    function fill(){
-        $this->folers($this->tree);
-    }
+
 
      function connect():bool{
         try{
             $this->conn = new PDO("oci:dbname=".$this->tns.";charset=UTF8",$this->db_username,$this->db_password);
-
+            $this->get_folders($this->tree);
+            return true;
         }
         catch(Exception $exp){
             return false;
         }
-        return true;
+
     }
 
-    public function run(){
-        $this->folers($this->tree);
-    }
 
-    private function folers(emc $e){
+    private function get_folders(emc $e){
             $query=$this->conn->prepare("SELECT CONT_ID, COMP_ID, COMP_NAME FROM ST_GRC WHERE (CONT_ID='".$e->id."')");
             $query->execute();
             for($i=0; $row = $query->fetch(); $i++){
                 $locemc=new emc($row['COMP_NAME'],$row['COMP_ID']);
 
                 $e->add($locemc);
-                $this->items($locemc);
-                $this->folers($locemc);
+                //$this->get_items($locemc);
+                $this->get_folders($locemc);
             }
-
       }
-    private function items(emc $emc){
+
+    private function get_items(emc $emc){
         $query=$this->conn->prepare("SELECT GR_ID, POINT_ID, GR_NAME,POINT_CODE,POINT_NAME FROM ST_GRP WHERE (GR_ID = '".$emc->id."')");
         $query->execute();
         for($i=0; $row = $query->fetch(); $i++){
             $locit=new item($row['GR_ID'],$row['POINT_ID'],$row['GR_NAME'],$row['POINT_CODE'],$row['POINT_NAME']);
-           $this->item_schedule($locit);
+          // $this->item_schedule($locit);
             $emc->add_item($locit);
+            //$this->item_data($locit,"2017.09.19","2017.09.19");
         }
     }
 
-    private function item_schedule(item $item){
+    private function get_item_schedule(item $item){
         $query=$this->conn->prepare("SELECT ML_NAME, ML_ID, PL_ID, POINT_ID,AGGS_TYPE_NAME,AGGS_TYPE_ID,EU_CODE,MD_NAME,MD_ID FROM ST_PL WHERE (POINT_ID = '".$item->POINT_ID."') and (MD_ID=5 OR MD_ID=3 OR MD_ID=6 OR MD_ID=8) ORDER BY ML_ID");
         $query->execute();
         for($i=0; $row = $query->fetch(); $i++){
             $sch=new Schedule($row['ML_NAME'],$row['ML_ID'],$row['PL_ID'],$row['POINT_ID'],$row['AGGS_TYPE_NAME'],$row['AGGS_TYPE_ID'],$row['EU_CODE'],$row['MD_NAME'],$row['MD_ID']);
             $item->add_schedule($sch);
+
         }
+    }
+    ///format date time "2017.9.13"
+    public function get_item_data(item $item,string $datefrom,string $dateto):array{
+        $query=$this->conn->prepare("SELECT POINT_ID,ML_ID,ET I, VAL, nvl(PL_RV,VAL) DR,DSTATUS QUALITY".
+                " from ST_AR where DA>=to_date('".$datefrom."','YYYY.MM.DD') and DA<=to_date('".$dateto."','YYYY.MM.DD') and POINT_ID=".$item->POINT_ID." and ML_ID=49 order by I");
+        $query->execute();
+        $emcos_datas=array();
+        for($i=0; $row = $query->fetch(); $i++){
+            $colemd=new EmcosData();
+            $colemd->date=$row['I'];
+            $colemd->value=$row['VAL'];
+            $colemd->quality=$row['QUALITY'];
+            $colemd->ml_id=$row['ML_ID'];
+            $colemd->point_id=$row['POINT_ID'];
+            array_push($emcos_datas,$colemd);
+        }
+        return $emcos_datas;
     }
 
 }
@@ -148,5 +164,13 @@ class emc
    }
 }
 
+class EmcosData{
+        public $date;
+        public $point_id;
+        public $ml_id;
+        public $quality;
+        public $value;
+        public $dr;
+}
 
 ?>
